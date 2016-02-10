52c5327ec12bdac7942fde80006e6af994a9030f
Steven Myers <slmyers@ualberta.ca>
2016-02-10 02:57:22 -0700

    working on login dialog

--- frontend/web/app/controllers/loginController.js
+++ frontend/web/app/controllers/loginController.js
@@ -1,11 +1,21 @@
 // web token stuff not done yet
 
 angular.module('chemGeno')
-.controller('loginController', ['$scope', '$mdDialog', function($scope, $mdDialog) {
-  var originatorEv;
-  this.openMenu = function($mdOpenMenu, ev) {
-    originatorEv = ev;
-    $mdOpenMenu(ev);
+.controller('loginController', ['$scope','$mdMedia', '$mdDialog', function($scope, $mdMedia, $mdDialog) {
+  $scope.login = function($event) {
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+    $mdDialog.show({
+      controller: loginController,
+      templateUrl: 'dialog1.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen
+    });
+    $scope.$watch(function() {
+      return $mdMedia('xs') || $mdMedia('sm');
+    }, function(wantsFullScreen) {
+      $scope.customFullscreen = (wantsFullScreen === true);
+    });
   };
-          
 }]);
