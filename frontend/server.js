b28363a519552a17f1f3acb27d12168e3ae0d827
Steven Myers <slmyers@ualberta.ca>
2016-02-09 20:52:12 -0700

    initial commit

---
+++ frontend/server.js
@@ -0,0 +1,9 @@
+var express = require('express'),
+    app     = express();
+
+app.use(express.static('./web'));
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + 'web/index.html');
+});
+app.listen(5000);
+console.log('listening on localhost:5000');
