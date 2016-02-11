b1787d2203aedcfd92d1255f6e09a3722d732b7c
Steven Myers <slmyers@ualberta.ca>
2016-02-11 02:52:31 -0700

    got rid of databases and logfiles

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
     resources(:contact_us_tickets)
   end
 
+  post 'authenticate' => 'auth#authenticate'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
