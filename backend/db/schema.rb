e80c8d0873bdd0b17bd0a1ff1962d31e519531f8
Steven Myers <slmyers@ualberta.ca>
2016-02-13 13:55:49 -0700

    added email to user

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211063209) do
+ActiveRecord::Schema.define(version: 20160213205217) do
 
   create_table "contact_us_tickets", force: :cascade do |t|
     t.string   "email"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160211063209) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "email"
   end
 
 end
