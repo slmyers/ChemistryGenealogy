c9f6de1a450bb85bf424ba57d4ca992d6b425d3b
Steven Myers <slmyers@ualberta.ca>
2016-02-13 13:43:05 -0700

    back to original db state

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :username, :password, :email, :password_digest, presence: true
+  validates :username, :password, :password_digest, presence: true
 
   def User.find_by_credentials(username, password)
     return User.find_by username: username, password: password
