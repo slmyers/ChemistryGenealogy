0788e4fb970a1db77b1db4945ea03331a0ae4278
Steven Myers <slmyers@ualberta.ca>
2016-02-18 03:59:26 -0700

    got rid of puts

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -1,7 +1,5 @@
 class AuthController < ApplicationController
   def authenticate
-    puts params[:email]
-    puts params[:password]
     user = User.find_by_credentials(params[:email], params[:password])
     if user
       render json: authentication_payload(user)
