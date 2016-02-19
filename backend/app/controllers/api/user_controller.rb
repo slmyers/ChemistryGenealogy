8aae62c8ca6eef2fe29d025a8f10a7ea7076b6ad
Steven Myers <slmyers@ualberta.ca>
2016-02-19 15:01:52 -0700

    registration working

--- backend/app/controllers/api/user_controller.rb
+++ backend/app/controllers/api/user_controller.rb
@@ -11,7 +11,7 @@ class Api::UserController < ApplicationController
   end
 
   def create
-    @user = User.new_user(params[:password], params[:email], params[:first_name], param[:last_name])
+    @user = User.new_user(params[:password], params[:email], params[:first_name], params[:last_name])
     if @user != nil && @user.save
       render json: @user.as_json, status: :created
     else
@@ -39,7 +39,7 @@ class Api::UserController < ApplicationController
   end
 
   # this code is generated earlier and doens't contribute on the actual web app
-  
+
   private
 
   def user_params
