aa88d4c59cc7b2ee5ec49b8c2aea466d04fb9df4
Steven Myers <slmyers@ualberta.ca>
2016-02-18 22:42:42 -0700

    adjust layout of toolbar

--- frontend/web/app/services/registerService.js
+++ frontend/web/app/services/registerService.js
@@ -1,13 +1,5 @@
 angular.module('chemGeno')
 .service('registerService', function(store, $q, $http) {
-  // sanity check
-  var _checkReturnedFields = function(returnedUsername, inputUser) {
-    if(returnedUsername !== inputUser.username) {
-      return false;
-    }
-    return true;
-  };
-
   var register = function(user) {
     var d = $q.defer();
     return $http({
@@ -16,14 +8,9 @@ angular.module('chemGeno')
       url: 'http://localhost:3000/api/user',
       data: {email: user.email, password: user.password}
     }).success( function(res){
-      console.log('success');
+      console.log('register success');
       console.log(res);
-      if(!_checkReturnedFields(res.username, user)){
-        d.reject({error: "expected returned username: " + user.username + " got: " + res.data.username})
-      } else {
-        d.resolve(res.user);
-      }
-
+      d.resolve(res.user);
     }).error( function(res){
       d.reject(res.error);
     });
