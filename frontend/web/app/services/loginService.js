3d320e4eaa703a1ce4cf44fe31636f2fc2d66296
ctevans <cevans2@ualberta.ca>
2016-02-20 17:28:32 -0700

    more beautification everywhere

--- frontend/web/app/services/loginService.js
+++ frontend/web/app/services/loginService.js
@@ -1,44 +1,84 @@
+/**
+ * Controller for the User Dialogue
+ *
+ * Status: CONTROLLER
+ * Associated files: userDialog.html,
+ *
+ * The user dialog controller is associated with registration and submission of login information. So it is
+ * slightly associated with the User Controller (userController.js), yet is kept distinct for clarity of functions.
+ *
+ *
+ */
+
 angular.module('chemGeno')
 .service('loginService', function(store, $q, $http) {
-  var userNamespace = '401ChemGenoUser'
 
-  var userLoggedIn = function() {
-    if (getUser() !== null) {
-      return true;
-    } else {
-      return false;
-    }
-  };
 
-  var getUser = function() {
-    return store.get(userNamespace);
-  };
+      var userNamespace = '401ChemGenoUser'
 
-  var login = function(user) {
-    var d = $q.defer();
-    return $http({
-      header: 'Content-Type: application/json',
-      method: 'POST',
-      url: 'http://localhost:3000/authenticate',
-      data: {email: user.email, password: user.password}
-    }).success(function(resp) {
-      store.set(userNamespace, resp);
-      d.resolve(resp.user);
-    }).error(function(resp) {
-      console.log(resp);
-      d.reject(resp.error);
-    });
-    return d.promise;
-  };
+    /**
+     * When evoked will check if the user can be obtained, if the user is not equal to null we must have a user logged
+     * into our app. Otherwise return false (if null), as we do not have a user.
+     *
+     * @returns {boolean} If the user is logged in (true) or not (false).
+     */
+      var userLoggedIn = function() {
+       if (getUser() !== null) {
+          return true;
+        } else {
+          return false;
+        }
+      };
 
-  var logout = function() {
-    store.remove(userNamespace);
-  }
+    /**
+     *  Obtain from the store (What?) the value of the userNamespace.
+     * @returns {*} The value of the current user
+     */
+      var getUser = function() {
+        return store.get(userNamespace);
+      };
 
-  return  {
-    userLoggedIn: userLoggedIn,
-    login: login,
-    getUser: getUser,
-    logout: logout
-  };
+    /**
+     * A service function that when invoked will attempt to log the user in. If it succeeds then we set the store
+     * to the user, else we will send an error back to the user of the app.
+     *
+     * @param user A user of the app.
+     * @returns {*} The promise of the user being logged in.
+     */
+      var login = function(user) {
+        var d = $q.defer();
+        return $http({
+          header: 'Content-Type: application/json',
+          method: 'POST',
+          url: 'http://localhost:3000/authenticate',
+          data: {email: user.email, password: user.password}
+        }).success(function(resp) {
+          store.set(userNamespace, resp);
+          d.resolve(resp.user);
+        }).error(function(resp) {
+          console.log(resp);
+          d.reject(resp.error);
+        });
+        return d.promise;
+      };
+
+    /**
+     * Logs out the user, by removing the
+     * Local storage for the store. Eg: if we're in chrome and we went to the cosnole and the resources then we'll find something on localhost:5000 then we'll see it there.
+     * tl;dr: Store is the local storage within the user's browser of the application that is being used.
+     * This function removes this person from this local storage.
+     */
+      var logout = function() {
+        store.remove(userNamespace);
+      };
+
+    /**
+     * A bunch of return stuff (???????????) that will state what is going on with the state of the application.
+     */
+      return  {
+        userLoggedIn: userLoggedIn,
+        login: login,
+        getUser: getUser,
+        logout: logout
+      };
 });
--- frontend/web/app/services/registerService.js
+++ frontend/web/app/services/registerService.js
@@ -1,36 +1,57 @@
+/**
+ * Register Service
+ *
+ * Status: SERVICE
+ * Associated files:
+ *
+ * The Register service is associated with storing and handling the local information services in the app.
+ *
+ *
+ */
+
 angular.module('chemGeno')
 .service('registerService', function(store, $q, $http) {
-  // sanity check
-  var _checkReturnedFields = function(returnedUsername, inputUser) {
-    if(returnedUsername !== inputUser.username) {
-      return false;
-    }
-    return true;
-  };
+        //Sanity check that we have the returned field is exactly as we'd expect.
+        var _checkReturnedFields = function(returnedUsername, inputUser) {
+          if(returnedUsername !== inputUser.username) {
+            return false;
+          }
+          return true;
+        };
 
-  var register = function(user) {
-    var d = $q.defer();
-    return $http({
-      header: 'Content-Type: application/json',
-      method: 'POST',
-      url: 'http://localhost:3000/api/user',
-      data: {email: user.email, password: user.password, first_name: user.firstName, last_name: user.lastName}
-    }).success( function(res){
-      console.log('success');
-      console.log(res);
-      if(!_checkReturnedFields(res.username, user)){
-        d.reject({error: "expected returned username: " + user.username + " got: " + res.data.username})
-      } else {
-        d.resolve(res.user);
-      }
+    /**
+     * A registration function service that will be involved with the actual in depth placement of the user into the
+     * system.
+     *
+     * @param user A user object to register in our application.
+     * @returns {*} Returns a promise associated with the registration of the user.
+     */
+        var register = function(user) {
+          var d = $q.defer();
+          return $http({
+            header: 'Content-Type: application/json',
+            method: 'POST',
+            url: 'http://localhost:3000/api/user',
+            data: {email: user.email, password: user.password, first_name: user.firstName, last_name: user.lastName}
+          }).success( function(res){
+            console.log('success');
+            console.log(res);
+            if(!_checkReturnedFields(res.username, user)){
+              d.reject({error: "expected returned username: " + user.username + " got: " + res.data.username})
+            } else {
+              d.resolve(res.user);
+            }
 
-    }).error( function(res){
-      d.reject(res.error);
-    });
-    return d.promise;
-  }
+          }).error( function(res){
+            d.reject(res.error);
+          });
+          return d.promise;
+        }
 
-  return {
-    register: register
-  };
+    /**
+     * Return the register object as being (successfully) "register"ed)
+     */
+        return {
+          register: register
+        };
 });
