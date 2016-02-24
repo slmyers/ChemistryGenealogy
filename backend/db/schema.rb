abb235c8f97c7b42a59b8e7c294350abeeaa8215
Steven Myers <slmyers@ualberta.ca>
2016-02-24 00:04:29 -0700

    forgot admins table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224064530) do
+ActiveRecord::Schema.define(version: 20160224070335) do
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "users_id"
+  end
+
+  add_index "admins", ["users_id"], name: "index_admins_on_users_id"
 
   create_table "degree", force: :cascade do |t|
     t.date     "year"
