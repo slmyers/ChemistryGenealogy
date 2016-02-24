a14df6de3e66e8ea080001fd7a64b417ee02913e
Steven Myers <slmyers@ualberta.ca>
2016-02-23 17:42:06 -0700

    chenged registration flow for acct approval

--- frontend/web/app/controllers/userDialogController.js
+++ frontend/web/app/controllers/userDialogController.js
@@ -11,8 +11,8 @@
  */
 angular.module('chemGeno')
 //Stating that this is a controller as well as the fact that this takes in certain information.
-.controller('userDialogController', ['$scope', '$mdDialog', 'loginService', 'registerService',
-    function($scope, $mdDialog, loginService, registerService) {
+.controller('userDialogController', ['$scope', '$mdDialog', 'loginService', 'registerService', '$mdToast',
+    function($scope, $mdDialog, loginService, registerService, $mdToast) {
       $scope.master = {};
       //Registered user is initialize to be empty.
       $scope.registerUser = {};
@@ -22,18 +22,15 @@ angular.module('chemGeno')
       $scope.invalidLogin = false;
       //True if user clicks the registration button
       $scope.registration = false;
+      // start this as true so that we get to the login screen
+      $scope.login = true;
       //True upon recieving an error for a registration request
       $scope.invalidRegistration = false;
       //True upon sucessfuling posting a new user -- sets html logic to display
       //a message about registration process
       $scope.successRegistration = false;
 
-        /**
-         * Function associated with navigating through the processes of actually logging in a user from
-         * start to finish of this application process.
-         *
-         * @param loginUser Takes in a user to be logged into the app.
-         */
+
       $scope.submitLogin = function(loginUser) {
         console.log(loginUser);
         var promise = loginService.login(loginUser);
@@ -49,18 +46,13 @@ angular.module('chemGeno')
         });
       };
 
-        /**
-         * Function associated with navigating through the process of registering a user through our application.
-         *
-         * @param registerUser Takes in an object of a user to be registered to the app.
-         */
+
       $scope.submitRegistration = function(registerUser) {
         var promise = registerService.register(registerUser);
         promise.then(function(res) {
           console.log('success registration');
           console.log(res);
-          $scope.invalidRegistration = false;
-          $scope.successRegistration = true;
+          $scope.registrationConfirmation();
         }, function(error){
           console.log('error in registration');
           console.log(error);
@@ -69,26 +61,30 @@ angular.module('chemGeno')
         })
       };
 
-        /**
-         * Function associated with cancellation of the login process for the user.
-         */
+      $scope.registrationConfirmation = function() {
+        $mdDialog.cancel();
+        $mdToast.show(
+          $mdToast.simple()
+          .textContent("We'll contact you at " + $scope.registerUser.email + " upon account approval!")
+          .position('top right')
+          .hideDelay(3000)
+        );
+      };
+
       $scope.cancelLogin = function() {
-        $mdDialog.cancel(); //Hide the current dialogue and break the promise returned from the md.show evocation.
+        $mdDialog.cancel();
         $scope.loginUser = angular.copy($scope.master); //revert the login user to the value of the master variable.
       };
 
-        /**
-         * Function associated with the cancellation of the reigstration process of a new user.
-         */
-        $scope.cancelRegistration = function() {
-        $mdDialog.cancel(); //Hide the current dialogue and break the promise returned from the md.show evocation.
+
+      $scope.cancelRegistration = function() {
+        $mdDialog.cancel();
         $scope.registerUser = angular.copy($scope.master); //revert register user to value of the master variable.
       };
 
-        /**
-         * When invoked, this function will set a trifecta of boolean flags to state the current state of the app.
-         */
+      // changes UI state to transition to the registration view
       $scope.toRegistrationView = function() {
+        $scope.login = false;
         $scope.invalidLogin = false; //No invalid login.
         $scope.invalidRegistration = false; //No invalid registration.
         $scope.registration = true; //Registration is confirmed, set to true.
