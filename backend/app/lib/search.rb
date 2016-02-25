572c689287d39c887ac1bc8894e250dca100867a
Steven Myers <slmyers@ualberta.ca>
2016-02-25 13:32:32 -0700

    got person instantiable

--- backend/app/lib/search.rb
+++ backend/app/lib/search.rb
@@ -1,17 +1,3 @@
 class Search
-  @type
-
-  def initialize(type)
-    @type = type
-  end
-
-  def guard(type)
-    if type != 'PERSON' || type != 'INSTITUTION'
-      return false
-    end
-    return true
-  end
-
-
 
 end
--- backend/reloadDB.sh
+++ backend/reloadDB.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # uses migrations to construct database then seeds database with test data
+rake db:drop
 rake db:migrate
 rake db:schema:load
 rake db:seed RAILS_ENV=development
