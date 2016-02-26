c85c82ec72b737cc84046c2c0ed011f333ddfb18
Steven Myers <slmyers@ualberta.ca>
2016-02-25 17:23:28 -0700

    person looks ok

--- backend/spec/models/person_spec.rb
+++ backend/spec/models/person_spec.rb
@@ -1,10 +1,18 @@
-# kind of pointless test, but this really just ensures that the testing
-# framework is properly set up.
 require_relative '../../app/models/api/person.rb'
 require_relative '../../app/models/api.rb'
 
+
+# sanity checks
 describe Person do
   it 'is instantiable' do
     expect(Person.new).not_to eql(nil)
   end
+
+# uncomment if you want to pollute db
+#  it 'is saveable' do
+#    @person = Person.new( name: "Steven Myers", position: "professor", approved: true,
+#                          institution_id: 2)
+#    expect(@person.save()).not_to eql(nil)
+#    expect(Person.exists?(:name => "Steven Myers")).to be(true)
+#  end
 end
