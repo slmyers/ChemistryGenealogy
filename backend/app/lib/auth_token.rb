30704c11ab71f2d4e619e2b003e4f804815ab9fa
Steven Myers <slmyers@ualberta.ca>
2016-02-28 13:28:12 -0700

    removed secret key

--- backend/app/lib/auth_token.rb
+++ backend/app/lib/auth_token.rb
@@ -3,15 +3,18 @@ class AuthToken
   # token stays valid for a month
   def self.encode(payload, ttl_in_minutes = 60 * 24 * 30)
     payload[:exp] = ttl_in_minutes.minutes.from_now.to_i
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    # https://github.com/jwt/ruby-jwt/blob/master/lib/jwt.rb#L88
+    # can use Rails.application.secrets.secret_key_base as key base
+    # but using empty string to reduce dependencies/make deployment easier/
+    # ensure it's runnable by other members
+    JWT.encode(payload, '')
   end
 
   # Decode a token and return the payload inside
   # If will throw an error if expired or invalid. See the docs for the JWT gem.
   def self.decode(token, leeway = nil)
-    decoded = JWT.decode(token, Rails.application.secrets.secret_key_base, leeway: leeway)
+    # https://github.com/jwt/ruby-jwt/blob/master/lib/jwt.rb#L97
+    decoded = JWT.decode(token, '''', leeway: leeway)
     HashWithIndifferentAccess.new(decoded[0])
   end
-
-  
 end
