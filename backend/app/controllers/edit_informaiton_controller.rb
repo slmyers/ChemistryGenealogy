dfa8053f4c172a58c301f484d0453c7135b74946
jihwan <jihwan@ualberta.ca>
2016-02-28 16:00:02 -0700

    rails generated controller for edit information

---
+++ backend/app/controllers/edit_informaiton_controller.rb
@@ -0,0 +1,6 @@
+class EditInformaitonController < ApplicationController
+	# editing information won't need a user to track if the information exists
+	# We assume the information being edit pre exists
+	def edit
+	end
+end
