d6aab3370694633b91a9de04064a6af351d446d5
jihwan <jihwan@ualberta.ca>
2016-02-28 16:58:12 -0700

    Some conditional control flows added

--- backend/app/controllers/informaiton_controller.rb
+++ backend/app/controllers/informaiton_controller.rb
@@ -1,18 +1,27 @@
 class InformaitonController < ApplicationController
 	# editing information won't need a user to track if the information exists
 	# We assume the information being edit pre exists
-	# degrees, institutions, mentors, people, postdocs should be editted
+	# degrees, institutions, mentors, postdocs should be editted
 	# user informtion such as name should be editted too
 	def submit_information
+		# First time creating a post
+		# check it params degrees and institutions are not present
+		if !param[:degrees].present? || !param[:institutions].present?
+			render json: { errors: ['degrees and institutions are required fields'] }, status: :bad_request
+		elsif !param[:mentors].present? || !param[:postdocs].present?
+			# if mentors and postdocs fields are left blank, due to some reasons
+			# leave it blank until the user edits.
+		
 	end
 
 	def edit_information
 		# Assume parameters with no value in it will not replace the old params
-		if !params.has_key(:degree)
+		if !params.has_key(:degrees)
 	end
 
 	def edit_user
 		# user should be able to edit their credentials
+		# Or do we want to implement this?
 	end
 
 	def notify_admin
