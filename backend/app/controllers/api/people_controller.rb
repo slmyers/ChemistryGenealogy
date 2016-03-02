28f87d52f8cd75707ef4e4465cb745c63a8a86f8
jihwan <jihwan@ualberta.ca>
2016-03-02 00:02:50 -0700

    updated people controller from person model

--- backend/app/controllers/api/people_controller.rb
+++ backend/app/controllers/api/people_controller.rb
@@ -10,14 +10,25 @@ class Api::PeopleController < ApiController
   end
 
   def new
+    # differences between new and create?
     render json: {warning: 'not implemented'}, status: 200
   end
 
   def edit
+    # differences between edit and update?
     render json: {warning: 'not implemented'}, status: 200
   end
 
   def create
+    # creating a new user, but do we want to check if user already exists?
+    Rails.logger.info(params)
+    # check if parameter is there or null
+    if params.has_key?(:name) && params.has_key?(:position) && params.has_key?(:institution)
+      unless Person.exists?(:name) # check if the person already in db
+        person = Person.new_person(params[:name], params[:position], params[:institution])
+        person.save
+        # hmm call render json?
+        return
     render json: {warning: 'not implemented'}, status: 200
   end
 
