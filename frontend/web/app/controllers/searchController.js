acf9f179e7ab3cfdd0d97b4290305767cc5d1e88
ConnorSheremeta <thecon21@gmail.com>
2016-03-03 14:59:32 -0700

    trying to combine both arrays

--- frontend/web/app/controllers/searchController.js
+++ frontend/web/app/controllers/searchController.js
@@ -10,7 +10,7 @@ angular.module('chemGeno')
             $scope.getInstitution = function(id) {
                 var arrayLength = $scope.institutions.length;
                 for (var i = 0; i < arrayLength; i++) {
-                    
+
                     if ($scope.institutions[i].institution_id == id){
                         return $scope.institutions[i].name;
                     }
@@ -18,6 +18,24 @@ angular.module('chemGeno')
 
 
                 return "unknown";
+            };
+
+            $scope.joinData = function() {
+                var peopleArrayLength = $scope.people.length;
+                for (var i = 0; i < peopleArrayLength; i++) {
+                    var institutionsArrayLength = $scope.institutions.length;
+                    for (var j = 0; j < institutionsArrayLength; j++) {
+
+                        if ($scope.institutions[j].id == $scope.people[i].institution_id) {
+                            $scope.people[i].institution_id = $scope.institutions[j].name
+
+                        }
+
+                    }
+
+
+                }
+                return;
             };
 
 
