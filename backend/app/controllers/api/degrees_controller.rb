6755ba7872251c1c359d6f2bada07fe113cf52e8
theresa-cy-ngo <theresa.cy.ngo@gmail.com>
2016-03-04 01:31:29 -0700

    worked on backend controllers, not tested yet

--- backend/app/controllers/api/degrees_controller.rb
+++ backend/app/controllers/api/degrees_controller.rb
@@ -1,3 +1,6 @@
+# temporarily filled out but not used yet (just AggregatedController used)
+# will have to figure out how to pass the json parameters to this controller
+
 class Api::DegreesController < ApiController
   respond_to :json
 
@@ -17,13 +20,26 @@ class Api::DegreesController < ApiController
     render json: {warning: 'not implemented'}, status: 200
   end
 
+  # this will be called to create a new degree
+  # parameters from JSON: year, type, institution
+  # will likely need to be called each time a degree needs to be made?
+  # *** Will deal with handling the array in this controller later
+  # currently assumes that all parameters are not nil
+  # not sure if we should render as json
   def create
     Rails.logger.info(params)
-    # have to retrieve the array degreeInformation from submitFileObject
-    if params.has_key?(:year) && params.has_key?(:degree_type)
-      unless Degree.exists?(:degree_type)
-        # need to find the way to find institution id so i can pass it as a parameter
-    render json: {warning: 'not implemented'}, status: 200
+    if params.has_key?(:year) && params.has_key?(:type) && params.has_key?(:institution)
+      # need to find the institution id first
+      instit_id = Institution.find_institution_id(params[:institution])
+      # check if the degree exists first
+      unless Degree.exists?(year: params[:year], degree_type: params[:type], :institution_id => instit_id)
+        @degree = Degree.new_degree(params[:year], params[:type], params[:institution])
+        if @degree != nil && @degree.save
+          render json: @degree.as_json, status: :created
+          return
+        end
+      end
+    end
   end
 
   def update
