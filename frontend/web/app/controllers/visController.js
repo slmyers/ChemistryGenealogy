db3b60452c51ebee8cfb0f4c5307e24d8eb7eee4
shikitan <dshin@ualberta.ca>
2016-03-11 11:38:39 -0700

    pushing my updated branch

--- frontend/web/app/controllers/visController.js
+++ frontend/web/app/controllers/visController.js
@@ -17,4 +17,40 @@ function($scope) {
   };
 
   $scope.estimateDimensions();
+
+ // Create a new directed graph
+  var g = new dagreD3.graphlib.Graph().setGraph({});
+
+  //two nodes, two paths NOTE THE INCLUDED 'weight' element for edges
+var nodes = [ "Todd L Lowary", "Ole Hindsgaul" ];
+// Automatically label each of the nodes
+nodes.forEach(function(name) { g.setNode(name, { label: name }); });
+
+var edges = [{u:"Ole Hindsgaul",v:"Todd L. Lowary",value:{label:"O to T"}}];
+edges.forEach(function(edge) {
+  g.setEdge(edge.u, edge.v, edge.value); });
+
+  // Create the renderer
+  var render = new dagreD3.render();
+
+// Set up an SVG group so that we can translate the final graph.
+var svg = d3.select('svg'),
+    svgGroup = svg.append('g');
+
+// Set up zoom support
+var zoom = d3.behavior.zoom().on("zoom", function() {
+      svgGroup.attr("transform", "translate(" + d3.event.translate + ")" +
+                                  "scale(" + d3.event.scale + ")");
+    });
+svg.call(zoom);
+
+
+// Run the renderer. This is what draws the final graph.
+render(svgGroup, g);
+
+// Center the graph
+var xCenterOffset = (svg.attr('width') - layout.graph().width * initialScale) / 2;
+svgGroup.attr('transform', 'translate(' + xCenterOffset + ', 20)');
+svg.attr('height', layout.graph().height * initialScale + 40);
+
 }]);
