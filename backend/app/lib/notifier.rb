59ffadaa5163eab19fc970968ea0b79acaec89bf
Steven Myers <slmyers@ualberta.ca>
2016-03-13 15:30:16 -0600

    cleaner notification code

--- backend/app/lib/notifier.rb
+++ backend/app/lib/notifier.rb
@@ -1,4 +1,3 @@
-require 'set'
 class Notifier
 
   #this method will bundle all unapproved people (new people) together in a
@@ -25,132 +24,79 @@ class Notifier
     # an active relation
     @res_array = Array.new
     @unapproved_people.each do |p|
-      @excluded_mentorship_ids = Array.new
-      @excluded_supervision_ids = Array.new
+      @target = {'person' => p, 'person_institution' => p.institution}
 
-      @supervisors = Array.new
-      @mentors = Array.new
-      @supervisions = Array.new
-      @mentorships = Array.new
-      @institutions = Set.new
-      @degrees = Array.new
-
-      # got the person + institution
-      @person = p
-      @person_institution = p.institution
-      @institutions.add(p.institution)
 
       # got the supervisor, supervision, degree + institutions
-      p.supervisions.each do |s|
-        @supervisors.push(s.supervisor)
-        @supervisions.push(s)
-        @degrees.push(s.degree)
-        @institutions.add(s.supervisor.institution)
-        @institutions.add(s.degree.institution)
-        @excluded_supervision_ids.push(s.id)
-      end
+      @supervisors = self.supervisors(p.supervisions)
 
       # got the mentors, mentorships + institutions
-      p.mentorships.each do |m|
-        @mentors.push(m.mentor)
-        @mentorships.push(m)
-        @institutions.add(m.mentor.institution)
-        @institutions.add(m.institution)
-        @excluded_mentorship_ids.push(m.id)
-      end
+      @mentors = self.mentors(p.mentorships)
 
-      # got the supervisions + instituitons of p where p is the supervisor
-      @bad_name_supervised = @unapproved_supervisions.where(:supervisor_id => p.id)
-                            .includes(degree: :institution)
-                            .includes(person: :institution)
-      @supervised = Array.new
-      @bad_name_supervised.each do |s|
-        @supervisions.push(s)
-        @supervised.push(s.person)
-        @degrees.push(s.degree)
-        @instituitons.add(s.degree.institution)
-        @instituitons.add(s.person.institution)
-      end
+      # get the person's supervisions, supervised, degrees + instituitons
+      # suffix raw implies that the data still needs to be processed into a more
+      # friendly form.
+      @supervised_raw = @unapproved_supervisions.where(:supervisor_id => p.id)
+                        .includes(degree: :institution)
+                        .includes(person: :institution)
+      @supervised = self.supervisors(@supervised_raw)
 
-      @bad_name_mentored = @unapproved_mentorships.where(:mentor_id => p.id)
-                            .includes(:institution)
-                            .includes(person: :institution)
-      # got the mentorships + instituitons of p where p is the mentor
-      @bad_name_mentored.each do |m|
-        @mentorships.push(m)
-        @mentored.push(m.person)
-        @instituitons.add(m.instituiton)
-        @instituitons.add(m.person.instituiton)
-      end
-
+      @mentored_raw = @unapproved_mentorships.where(:mentor_id => p.id)
+                      .includes(:institution)
+                      .includes(person: :institution)
+      @mentored = self.mentors(@mentored_raw)
 
       @aggregated_person =
         {
-          'target' => @person,
+          'target' => @target,
           'mentors' => @mentors,
-          'mentorships' => @mentorships,
-          'supervisors' => @supervisors,
-          'supervisions' => @supervisions,
-          'supervised' => @supervised,
           'mentored' => @mentored,
-          'institutions' => @institutions
+          'supervisors' => @supervisors,
+          'supervised' => @supervised
         }
       @res_array.push(@aggregated_person)
-      self.test_includes(p)
     end
-    #return @res_array
-  end
-
-  def self.test_includes(person)
-    person.mentorships.each do |m|
-      puts m.mentor
-    end
+    return @res_array
   end
 
   # find unapproved mentorships except those withc excluded_ids
   # the excluded ids are the mentorships that are associated with
   # unapproved people.
-  def self.mentorships(excluded_ids)
-    @unapproved_mentorships = Mentorship.where({:approved => false})
-                              .where.not(:id => excluded_ids)
-                              .includes(:institution)
-                              .includes(person: :institution)
+  def self.mentors(unapproved_mentorships)
     @mentorships = Array.new
-    @institutions = Set.new
-    @people = Array.new
-    @unapproved_mentorships.each do |m|
-      @mentorships.push(m)
-      @people.push(m.person)
-      @institutions.add(m.person.institution)
-      @institutions.add(m.institution)
+    unapproved_mentorships.each do |m|
+      @mentorships_obj = {
+        'mentorship' => m,
+        'instituiton' => m.institution,
+        'mentor' => {
+          'person' => m.person,
+          'institution' => m.person.institution
+        }
+      }
+      @mentorships.push(@mentorships_obj)
     end
-
-    return {'mentorships' => @mentorships, 'people' => @people, 'institutions' => @institutions}
+    return @mentorships
   end
 
 
-  def self.supervisions(excluded_ids)
-    @unapproved_supervisions = Supervision.where({:approved => false})
-                              .where.not(:id => excluded_ids)
-                              .includes(degree: :institution)
-                              .includes(person: :institution)
-
+  def self.supervisors(unapproved_supervisions)
     @supervisions = Array.new
-    @degrees = Array.new
-    @people = Array.new
-    @institutions = Set.new
 
-    @unapproved_supervisions.each do |s|
-      @supervisions.push(s)
-      @people.push(s.person)
-      @degrees.push(s.degree)
-      @institutions.add(s.person.institution)
-      @institutions.add(s.degree.institution)
+    unapproved_supervisions.each do |s|
+      @supervision_obj = {
+        'supervision' => s,
+        'degree' => {
+          'degree' => s.degree,
+          'institution' => s.degree.institution
+        },
+        'supervisor' => {
+          'person' => s.person,
+          'institution' => s.person.institution
+        }
+      }
+      @supervisions.push(@supervision_obj)
     end
-
-    return {'supervisions' => @supervisions,
-            'degrees' => @degrees,
-            'people' => @people,
-            'institutions' => @institutions}
+    return @supervisions
   end
+
 end
