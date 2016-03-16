d7d20bc4aba5402dae1329e08c83448772873491
ctevans <cevans2@ualberta.ca>
2016-03-15 19:42:41 -0600

    changes?

--- frontend/web/app/services/viewService.js
+++ frontend/web/app/services/viewService.js
@@ -15,8 +15,7 @@ angular.module('chemGeno')
             return $http({
                 header: 'Content-Type: application/json',
                 method: 'GET',
-                url: 'http://localhost:3000/api/aggregated/:id',
-                params: {id: $stateParams.id}
+                url: 'http://localhost:3000/api/aggregated/'+ id.toString(),
             }).success(function (resp) {
                 d.resolve(resp);
             }).error(function (resp) {
