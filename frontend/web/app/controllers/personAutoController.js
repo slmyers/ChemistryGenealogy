3f9ef24aabbae1d6da25c599faa7d83620a47dfb
Steven Myers <slmyers@ualberta.ca>
2016-03-16 05:04:14 -0600

    can't figure out jsdocs

--- frontend/web/app/controllers/personAutoController.js
+++ frontend/web/app/controllers/personAutoController.js
@@ -1,13 +1,18 @@
 /**
- * This is a test.
+ * @module personAutoController
+ * @class personAutoController
  */
-
 angular.module('chemGeno')
 .controller('personAutoController', ['$scope', 'autoService', 'searchService', '$state',
 function($scope, autoService, searchService, $state) {
   $scope.selectedItem;
   $scope.searchText = '';
 
+  /**
+   *  @function myFunction stuff and more stuff
+   *  @memberof personAutoController
+   *  @instance
+   */
   $scope.selectedItemChange = function selectedItemChange(item) {
     searchService.executeSearch(item.name);
   };
