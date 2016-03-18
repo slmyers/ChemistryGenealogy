0fab89102ae70c6e11139cf7fbf6cfb28a5db987
ctevans <cevans2@ualberta.ca>
2016-03-18 10:23:41 -0600

    Adding frontend docs

---
+++ frontend/web/app/controllers/out/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "personAutoController"
+    ],
+    "modules": [
+        "personAutoController"
+    ],
+    "allModules": [
+        {
+            "displayName": "personAutoController",
+            "name": "personAutoController"
+        }
+    ],
+    "elements": []
+} };
+});
