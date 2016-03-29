731b2cda13dafdd5128aabddd4056aec2d498a46
ctevans <cevans2@ualberta.ca>
2016-03-28 21:14:31 -0600

    More protractor testsfor the main page

--- frontend/tests/e2e/search/SearchSpec.js
+++ frontend/tests/e2e/search/SearchSpec.js
@@ -13,6 +13,49 @@ describe('Search', function() {
       browser.get('http://localhost:5000/search');
       expect(element(by.id('theMainLoginButton')).isPresent()).toBe(true);
     });
+
+    it('has a search bar', function(){
+        browser.get('http://localhost:5000/search');
+        expect(element(by.id('peopleSearchInput')).isPresent()).toBe(true);
+
+
+    });
+
+    it('The search bar can accept text input and retains it', function(){
+        browser.get('http://localhost:5000/search');
+        //Enter "Todd" into the search bar.
+        element(by.id('peopleSearchInput')).sendKeys("Todd");
+
+        //Check that the search bar has the correct name in itself.
+        expect(element(by.id('peopleSearchInput')).getAttribute('value')).toEqual("Todd");
+
+    });
+
+    it('Has a data visualization', function(){
+        browser.get('http://localhost:5000/search');
+        expect(element(by.id('dataVis')).isPresent()).toBe(true);
+    });
+
+
+    it('Has the search options displayed', function(){
+        browser.get('http://localhost:5000/search');
+        //Enter "Todd" into the search bar.
+        element(by.id('peopleSearchInput')).sendKeys("Todd");
+
+        //Check that the search bar has the correct name in itself.
+        expect(element(by.id('peopleSearchInput')).getAttribute('value')).toEqual("Todd");
+
+
+        browser.waitForAngular();
+        expect(element(by.id('searchResultsArray')).isPresent()).toBe(true);
+        //searchResultsArray
+    });
+
+    it('Has a data visualization', function(){
+
+    });
+
+
     /*
     idk if you want to keep this stuff
     //Creates an instance of the LoginPage object, which holds most of the specific low level details of the tests.
